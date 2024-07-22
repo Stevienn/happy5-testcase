277e3893d841262b38aa52bccd4986df2e81d06a
stevienn <stevenhartanto0603@gmail.com>
2024-07-22 16:31:26 +0700

    chore: initial commit

---
+++ src/components/Roadmap/Roadmap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Roadmap = () => {
+  return (
+    <main className='main-roadmap'>
+
+    </main>
+    
+  )
+}
+
+export default Roadmap
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='main-sidebar'>
+
+    </div>
+  )
+}
+
+export default Sidebar
