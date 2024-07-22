41d20b01eff3262f1366d7d0531cc06e9e946633
stevienn <stevenhartanto0603@gmail.com>
2024-07-22 16:37:55 +0700

    feat: add sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='main-sidebar'>
-
+      <img src="/logo.png"
+        width={28}
+        height={28}
+        />
     </div>
   )
 }
